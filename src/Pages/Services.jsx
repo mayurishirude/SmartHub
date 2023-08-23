ee234422e972719d6dd57d6cf06a193216fd1656
amolkapadi <amolakapadi@gmail.com>
2023-08-23 21:39:24 +0530

    services add

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
+import Data from '../Data.json'
+const Services = () => {
+ 
+  const [activeTab, setActiveTab] = useState('Informationtechnology');
 
-function Services() {
-  return <div></div>;
-}
+  const filteredData = Data.filter(item => item.service_type === activeTab);
+
+  return (
+    <div className="container "> 
+    <h1>Services</h1>
+
+    <ul className="nav nav-tabs" id="myTabs">
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === 'Informationtechnology' ? 'active' : ''}`}
+          style={{ backgroundColor: activeTab === 'Informationtechnology' ? '#ffc451' : '' }} 
+          onClick={() => setActiveTab('Informationtechnology')}
+        >
+          Information Technology
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === 'civil' ? 'active' : ''}`}
+          style={{ backgroundColor: activeTab === 'civil' ? '#ffc451' : '' }} 
+          onClick={() => setActiveTab('civil')}
+        >
+           CIVIL TECHANOLOGY
+        </a>
+      </li>
+    </ul>
+
+    <div className="tab-content">
+      <div
+        className={`tab-pane fade ${activeTab === 'Informationtechnology' ? 'show active' : ''}`}
+        id="it-tab"
+      >
+        <ul>
+          {filteredData.map(service => (
+            <li key={service.id}>{service.service_name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={`tab-pane fade ${activeTab === 'civil' ? 'show active' : ''}`} id="civil-tab">
+        <ul>
+          {filteredData.map(service => (
+            <li key={service.id}>{service.service_name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  </div>
+  );
+};
 
 export default Services;
